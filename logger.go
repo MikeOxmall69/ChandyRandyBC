23023772fbdbf3eeca6d42df5fbaca05cf92376b
MikeOxmall69 <149020831+MikeOxmall69@users.noreply.github.com>
2023-10-25 18:34:00 -0400

    Update logger.go

--- logger.go
+++ logger.go
@@ -1,28 +1,78 @@
 package chandy_lamport
 
-import "container/list"
+import (
+	"fmt"
+	"log"
+)
 
-// Define a queue -- simple implementation over List
-type Queue struct {
-	elements *list.List
+// =================================
+//  Event logger, internal use only
+// =================================
+
+type Logger struct {
+	// index = time step
+	// value = events that occurred at that time step
+	events [][]LogEvent
 }
 
-func NewQueue() *Queue {
-	return &Queue{list.New()}
+type LogEvent struct {
+	serverId     string
+	// Number of tokens before execution of event
+	serverTokens int
+	event        interface{}
 }
 
-func (q *Queue) Empty() bool {
-	return (q.elements.Len() == 0)
+func (event LogEvent) String() string {
+	prependWithTokens := false
+	switch evt := event.event.(type) {
+	case SentMessageEvent:
+		switch evt.message.(type) {
+		case TokenMessage:
+			prependWithTokens = true
+		}
+	case ReceivedMessageEvent:
+		switch evt.message.(type) {
+		case TokenMessage:
+			prependWithTokens = true
+		}
+	case StartSnapshot:
+		prependWithTokens = true
+	case EndSnapshot:
+	default:
+		log.Fatal("Attempted to log unrecognized event: ", event.event)
+	}
+	if prependWithTokens {
+		return fmt.Sprintf("%v has %v token(s)\n\t%v",
+			event.serverId,
+			event.serverTokens,
+			event.event)
+	} else {
+		return fmt.Sprintf("%v", event.event)
+	}
 }
 
-func (q *Queue) Push(v interface{}) {
-	q.elements.PushFront(v)
+func NewLogger() *Logger {
+	return &Logger{make([][]LogEvent, 0)}
 }
 
-func (q *Queue) Pop() interface{} {
-	return q.elements.Remove(q.elements.Back())
+func (log *Logger) PrettyPrint() {
+	for epoch, events := range log.events {
+		if len(events) != 0 {
+			fmt.Printf("Time %v:\n", epoch)
+		}
+		for _, event := range events {
+			fmt.Printf("\t%v\n", event)
+		}
+	}
 }
 
-func (q *Queue) Peek() interface{} {
-	return q.elements.Back().Value
+func (log *Logger) NewEpoch() {
+	log.events = append(log.events, make([]LogEvent, 0))
+}
+
+func (logger *Logger) RecordEvent(server *Server, event interface{}) {
+	mostRecent := len(logger.events) - 1
+	events := logger.events[mostRecent]
+	events = append(events, LogEvent{server.Id, server.Tokens, event})
+	logger.events[mostRecent] = events
 }
